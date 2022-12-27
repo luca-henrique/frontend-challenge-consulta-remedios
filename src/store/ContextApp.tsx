7d8dda9cf49761185edae99ca5513ddf09e4a961
Lucas Henrique <lukas.paes18@gmail.com>
2022-12-26 22:51:09 -0300

    fix: context type

--- src/store/ContextApp.tsx
+++ src/store/ContextApp.tsx
@@ -1,11 +1,12 @@
 import {createContext, useState} from 'react';
+import {IProduct} from '../types';
 
 import {initialState, Props} from './state';
 
 export const ContextApp = createContext<Props>(initialState);
 
 export const AppProvider = ({children}: any) => {
-  const [cart, setCart] = useState<any>(initialState.cart);
-
+  const [cart] = useState<IProduct[]>(initialState.cart);
+  //@ts-ignore
   return <ContextApp.Provider value={{cart}}>{children}</ContextApp.Provider>;
 };
